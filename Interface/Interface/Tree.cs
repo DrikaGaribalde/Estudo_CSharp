a87581d8fbc8398d2a765487b68b983385e8c9f0
DrikaGaribalde <adrianagaribalde@gmail.com>
2020-08-24 21:36:08 -0300

    Implementando Interface

---
+++ Interface/Interface/Tree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interface {
+    class Tree : IAge{
+        private int rings; //número de anéis no tronco da árvore
+
+        //construtor
+        public Tree(int yearPlanted) {
+        //conta o número de anéis na árvore
+            rings = DateTime.Now.Year - yearPlanted;
+        }
+        //incrementa rings
+        public void AddRings() {
+            rings++;
+        }
+        //implementação da propriedade Age da interface
+        public int Age {
+            get { return rings; }
+        }
+        //propriedade Name da interface
+        public string Name {
+            get { return "Tree"; }
+        }
+    }
+}
