ebba470b7575703ca2ca78c77a30f2c6c9be49d6
DrikaGaribalde <adrianagaribalde@gmail.com>
2020-08-24 21:09:57 -0300

    Implementando uma Interface CSharp

---
+++ Interface/Interface/IAge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interface {
+
+    public interface IAge{
+        int Age{get;}
+        string Name {get;}
+    }
+}
+
+
---
+++ Interface/Interface/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interface {
+    class Person : IAge {
+        private string firstName;
+        private string lastName;
+        private int yearBorn;
+
+        //construtor
+        public Person(string firstNameValue, string lastNameValue, int yearBornValue) {
+            firstName = firstNameValue;
+            lastName = lastNameValue;
+
+            if (yearBornValue > 0 && yearBornValue <= DateTime.Now.Year) {
+                yearBorn = yearBornValue;
+            } else {
+                yearBorn = DateTime.Now.Year;
+            }
+        }
+        //implementação da propriedade Age da interface IAge
+        public int Age {
+            get { return DateTime.Now.Year - yearBorn; }
+        }
+        //propriedade Name da interface IAge
+        public string Name {
+            get { return firstName + " " + lastName; }
+        }
+    }
+}
