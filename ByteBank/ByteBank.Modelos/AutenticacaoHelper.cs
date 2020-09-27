ad73e2f684c10b4d585f8463af5e5b7556095dcb
DrikaGaribalde <adrianagaribalde@gmail.com>
2020-09-27 17:53:55 -0300

    Projeto ByteBank C#  Bibliotecas DLLs, documentação e usando o NuGet - Curso Alura

---
+++ ByteBank/ByteBank.Modelos/AutenticacaoHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ByteBank.Modelos {
+    class AutenticacaoHelper {
+        public bool CompararSenhas(string senhaVerdadeira, string senhaTentativa) {
+            
+            return senhaVerdadeira == senhaTentativa;
+        }
+    }
+}
